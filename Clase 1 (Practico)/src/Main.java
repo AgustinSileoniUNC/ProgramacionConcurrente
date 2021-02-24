61d6492f20e29a113da693d7d0025183043321ae
Agustin Sileoni <agustinsileoni@mi.unc.edu.ar>
2021-02-24 16:18:29 -0300

    Prueba con 2 hilos

---
+++ Clase 1 (Practico)/src/Main.java
@@ -0,0 +1,10 @@
+import tareas.Tarea;
+
+public class Main {
+    static boolean condicion = true;
+    public static void main(String[] args) throws Exception {
+
+        new Tarea();
+    
+    }
+}
